6ad5b2333d428e176907fb3a8d3d0b16653f6093
Evando Junior <evando.junior@live.com>
2017-08-25 17:10:12 -0300

    Init Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <header-component></header-component>
+    <router-view></router-view>
+    <header></header>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/template/Header'
+import FooterComponent from '@/components/template/Footer'
+export default {
+  components: {
+    HeaderComponent,
+    FooterComponent
+  },
+  name: 'app'
+}
+</script>
